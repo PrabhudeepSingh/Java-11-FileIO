adc7a87112e1e5e3b00765436af68ffae0b585e1
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-10-30 12:33:04 +0530

    Program to display all file names in a directory.

---
+++ DisplayFname.java
@@ -0,0 +1,23 @@
+import java.io.File;
+public class DisplayFname
+{
+   public static void main(String[] args)
+   {
+     int count=0;
+	 
+     File f=new File("C:\\Program Files");
+     String s[]=f.list();
+	 
+     for(String s1:s)
+     {
+        File f1=new File(f,s1);
+        if(f1.isFile())
+        {
+           count++;
+           System.out.println(s1);
+        }
+     } 
+    System.out.println("total number"+count);
+      
+  }
+}
